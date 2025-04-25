5e055083b09c2ffeff90df3c546cc09f4c7dba5b
Jaimie Bowen <jraebowen@gmail.com>
2025-04-25 13:45:11 -0700

    fix: fix positioning of image modal text and close button

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 //Card display on page load
-let initialCards = [
+const initialCards = [
   {
     name: "Maine Coon",
     link: "https://images.unsplash.com/photo-1573865526739-10659fec78a5?q=80&w=2630&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
