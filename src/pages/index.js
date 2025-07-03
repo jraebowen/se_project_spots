0336b6fbc6eea9a2b1193e769c42bbb7443fc51f
Jaimie Bowen <jraebowen@gmail.com>
2025-07-03 14:54:45 -0700

    update webpack configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
+import "./index.css";
+import profileSrc from "../images/profile-image.jpeg";
+
+const profileImage = document.getElementById("image-profile");
+profileImage.src = profileSrc;
+
 import {
   enableValidation,
   resetValidation,
   disableButton,
   validationConfig,
-} from "./validation.js";
+} from "../scripts/validation.js";
 
 //Card display on page load
 const initialCards = [
